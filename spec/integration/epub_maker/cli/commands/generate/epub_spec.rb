24ba18eae8d6d2943026fbf88d0eba5211be4866
Igor S. Morozov <igor@morozov.is>
2021-01-30 00:09:28 +0300

    Create valid epub with minimum metadata (#5)

--- spec/integration/epub_maker/cli/commands/generate/epub_spec.rb
+++ spec/integration/epub_maker/cli/commands/generate/epub_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 require 'epub_maker/cli'
+require 'tty/prompt/test'
 
 RSpec.describe EPUBMaker::CLI::Commands::Generate::EPUB do
-  subject(:run) { described_class.new.call(path: path) }
+  subject(:run) { command.call(path: path) }
+
+  let(:command) { described_class.new }
 
   let(:path) { Pathname.new(Tempfile.new.path) }
+  let(:stub_prompt) { TTY::Prompt::Test.new }
+
+  before do
+    stub_prompt.input << "\n\n\n"
+    stub_prompt.input.rewind
+    allow(command).to receive(:prompt).and_return(stub_prompt)
+  end
 
   it 'creates epub' do
     expect { run }.not_to raise_error
